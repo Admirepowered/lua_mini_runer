207982b6452c5e8b4e5a8e0d2034491b57bc84e8
Admire <43035036+Admirepowered@users.noreply.github.com>
2023-12-19 00:58:45 +0800

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,97 @@
+local curl = require "lcurl.safe"
+local json = require "cjson.safe"
+function request_curl(url,header,method,postdata,timeout1)
+if timeout1 == nil then
+timeout1=20
+end
+
+data = ""
+if 	method == "POST" then 
+	local c = curl.easy{
+        url = url,
+        post = 1,
+        postfields = postdata,
+        httpheader = header,
+        timeout = timeout1,
+        ssl_verifyhost = 0,
+        ssl_verifypeer = 0,
+        proxy = "",
+        writefunction = function(buffer)
+            data = data .. buffer
+            return #buffer
+        end,
+	}
+	local _, e = c:perform()
+	c:close()
+	if e then
+		return nil
+    end
+	return data
+elseif method == "HEADER" then
+	if postdata=="" then
+	follw=0
+	else
+	follw=1
+	end
+    local c = curl.easy {
+        url = url,
+        followlocation = follw,
+        header = 1,
+		--returntransfer = 1,
+        nobody = 1,
+        httpheader = header,
+        timeout = timeout1,
+        proxy = "",
+        writefunction = function(buffer)
+            data = data .. buffer
+            return #buffer
+        end,
+		headerfunction = function(buffer)
+            data = data .. buffer
+            return #buffer
+        end
+    }
+	--c:setopt(curl.OPT_HEADERFUNCTION, headerfunction)
+    local _, e = c:perform()
+    c:close()
+	
+    if e then
+        return nil
+    end
+    return data
+else
+	local c = curl.easy{
+	url = url,
+	followlocation = 1,
+	httpheader = header,
+	timeout = timeout1,
+	proxy = "",
+	writefunction = function(buffer)
+		data = data .. buffer
+		return #buffer
+	end,
+	}
+	local _, e = c:perform()
+	c:close()
+	if e then
+		return nil
+    end
+	return data
+end
+
+end
+
+
+function test()
+--local a =io.read()
+local url ="http://pan.baidu.com/api/filemetas"
+local header = { "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 
+(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36" }
+local data = request_curl(url,header)
+local j = json.decode(data)
+if j==nil then 
+	return "error data"
+end
+return j.request_id
+--return data
+end
