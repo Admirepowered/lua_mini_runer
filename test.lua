109d0165dc90a5bf13001c62e55a4c1d4a220f4c
Admire <43035036+Admirepowered@users.noreply.github.com>
2023-12-19 01:09:04 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -85,8 +85,7 @@ end
 function test()
 --local a =io.read()
 local url ="http://pan.baidu.com/api/filemetas"
-local header = { "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 
-(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36" }
+local header = { "User-Agent: Mozilla" }
 local data = request_curl(url,header)
 local j = json.decode(data)
 if j==nil then 
